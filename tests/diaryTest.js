607d05afb5f5ac863ca3b63942984229b2cfbd52
okezieobi <obiedereokezie@gmail.com>
2018-12-29 23:21:19 +0100

    [feature #162883631] tested and wrote code for delete-one-entry endpoint

--- tests/diaryTest.js
+++ tests/diaryTest.js
@@ -8,5 +8,29 @@ chai.use(chaiHttp);
 
 describe('Test diary endpoints at "/v1/entries" with (GET, POST) and "/v1/entries/:id" with (GET, PUT, DELETE)', () => {
   describe('Test diary endpoint at "/v1/entries" with GET and POST', () => {});
-  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {});
+  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {
+    describe("Test endpoint at '/v1/entries/:id to delete a diary entry with DELETE", () => {
+      it("it should delete a diary entry at '/v1/entries/:id' with DELETE if id exists", (done) => {
+        const id = 0;
+        chai.request(app)
+          .del(`/v1/entries/${id}`)
+          .then((res) => {
+            expect(res).to.have.status(200);
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.property('message').equal('Diary entry successfully deleted');
+            done();
+          });
+      });
+      it("should NOT delete a diary entry at '/v1/entries/:id' with DELETE if id does not exist", (done) => {
+        const id = 10;
+        chai.request(app)
+          .del(`/v1/entries/${id}`)
+          .then((res) => {
+            expect(res).to.have.status(404);
+            expect(res.body).to.have.property('message').equal('Diary entry does not exist');
+            done();
+          });
+      });
+    });
+  });
 });
