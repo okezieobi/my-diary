b9954601cd1148af079fc8130d4619d793015505
okezieobi <obiedereokezie@gmail.com>
2018-12-29 10:23:37 +0100

    #162879834 tested and wrote code for get-one endpoint

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -5,7 +5,20 @@ class DiaryController {
 
   static createOne() {}
 
-  static getOne() {}
+  static getOne(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const diaryEntry = diaryList.find(diaryItem => diaryItem.id === id);
+    if (diaryEntry) {
+      res.status(200).json({
+        message: 'Diary entry successfully retrieved',
+        diaryEntry,
+      });
+    } else {
+      res.status(404).json({
+        message: 'Diary entry does not exist',
+      });
+    }
+  }
 
   static updateOne() {}
 
--- tests/diaryTest.js
+++ tests/diaryTest.js
@@ -8,5 +8,21 @@ chai.use(chaiHttp);
 
 describe('Test diary endpoints at "/v1/entries" with (GET, POST) and "/v1/entries/:id" with (GET, PUT, DELETE)', () => {
   describe('Test diary endpoint at "/v1/entries" with GET and POST', () => {});
-  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {});
+  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {
+    describe('Test diary endpoint at "/v1/entries/:id" to get one entry with GET', () => {
+      it("should get a diary entry at '/v1/entries/:id' with GET if id exists", (done) => {
+        const id = 0;
+        chai.request(app)
+          .get(`/v1/entries/${id}`)
+          .then((res) => {
+            expect(res).to.have.status(200);
+            expect(res.body).be.an('object');
+            expect(res.body.diaryEntry).to.have.property('id').equal(0);
+            expect(res.body.diaryEntry).to.have.property('title').equal('breakfast');
+            expect(res.body.diaryEntry).to.have.property('description').equal('alone');
+            done();
+          });
+      });
+    });
+  });
 });
