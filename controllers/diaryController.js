22d84a74f2d249ea99c2fae329ad677dad8c899a
okezieobi <obiedereokezie@gmail.com>
2018-12-29 22:29:46 +0100

    [feature #162880154] tested and wrote update-one-entry endpoint

--- controllers/diaryController.js
+++ controllers/diaryController.js
@@ -7,7 +7,34 @@ class DiaryController {
 
   static getOne() {}
 
-  static updateOne() {}
+  static updateOne(req, res) {
+    const id = parseInt(req.params.id, 10);
+    const diaryEntry = diaryList.find(diaryItem => diaryItem.id === id);
+    if (!diaryEntry) {
+      res.status(404).json({
+        message: 'Diary entry does not exist',
+      });
+    } else if (!req.body.title) {
+      res.status(400).json({
+        message: 'Title is required',
+      });
+    } else if (!req.body.description) {
+      res.status(400).json({
+        message: 'Description is required',
+      });
+    } else {
+      const updatedDiaryEntry = {
+        id: diaryEntry.id,
+        title: req.body.title,
+        description: req.body.description,
+      };
+      diaryList.splice(diaryEntry.id, 1, updatedDiaryEntry);
+      res.status(200).json({
+        message: 'Diary entry updated successfully',
+        updatedDiaryEntry,
+      });
+    }
+  }
 
   static deleteOne() {}
 }
--- tests/diaryTest.js
+++ tests/diaryTest.js
@@ -8,5 +8,61 @@ chai.use(chaiHttp);
 
 describe('Test diary endpoints at "/v1/entries" with (GET, POST) and "/v1/entries/:id" with (GET, PUT, DELETE)', () => {
   describe('Test diary endpoint at "/v1/entries" with GET and POST', () => {});
-  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {});
+  describe('Test diary endpoints at "/v1/entries/:id" with GET, PUT, DELETE', () => {
+    describe('Test endpoint at "/v1/entries/:id" to update a diary entry with PUT', () => {
+      it("should update a diary entry at '/v1/entries/:id' with PUT if id exists", (done) => {
+        const id = 0;
+        chai.request(app)
+          .put(`/v1/entries/${id}`)
+          .send({
+            title: 'breakfast',
+            description: 'with buddy',
+          })
+          .then((res) => {
+            expect(res).to.have.status(200);
+            expect(res.body).be.an('object');
+            expect(res.body.updatedDiaryEntry).to.have.property('id').equal(0);
+            expect(res.body.updatedDiaryEntry).to.have.property('title').equal('breakfast');
+            expect(res.body.updatedDiaryEntry).to.have.property('description').equal('with buddy');
+            done();
+          });
+      });
+      it("should NOT update a diary entry at '/v1/entries/:id' if id does not exist with PUT", (done) => {
+        const id = 10;
+        chai.request(app)
+          .put(`/v1/entries/${id}`)
+          .then((res) => {
+            expect(res).to.have.status(404);
+            expect(res.body).to.have.property('message').equal('Diary entry does not exist');
+            done();
+          });
+      });
+      it("it should NOT update a diary entry at '/v1/entries' if title is null or undefined with PUT", (done) => {
+        const id = 0;
+        chai.request(app)
+          .put(`/v1/entries/${id}`)
+          .send({
+            description: 'lorem',
+          })
+          .then((res) => {
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property('message').equal('Title is required');
+            done();
+          });
+      });
+      it("it should NOT update a diary entry at '/v1/entries' if description is null or undefined with PUT", (done) => {
+        const id = 0;
+        chai.request(app)
+          .put(`/v1/entries/${id}`)
+          .send({
+            title: 'lorem',
+          })
+          .then((res) => {
+            expect(res).to.have.status(400);
+            expect(res.body).to.have.property('message').equal('Description is required');
+            done();
+          });
+      });
+    });
+  });
 });
